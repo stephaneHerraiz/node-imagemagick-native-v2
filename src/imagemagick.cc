2d11fc78e4959e8873d7ebfa0c5c7f437f2e4ce4
Herraiz Stéphane <stephane.herraiz@sixense-group.com>
2021-08-13 16:53:05 +0200

    feat(header): declarations move to header

--- src/imagemagick.cc
+++ src/imagemagick.cc
@@ -52,85 +52,13 @@ private:
     bool diskLimited;
 };
 
-// Base context for calls shared on sync and async code paths
-struct im_ctx_base {
-    Nan::Callback * callback;
-    std::string error;
-
-    char* srcData;
-    size_t length;
-    int debug;
-    int ignoreWarnings;
-    std::string srcFormat;
-
-    // generated blob by convert or composite
-    Magick::Blob dstBlob;
-
-    virtual ~im_ctx_base() {}
-};
-// Extra context for identify
-struct identify_im_ctx : im_ctx_base {
-    Magick::Image image;
-
-    identify_im_ctx() {}
-};
-// Extra context for convert
-struct convert_im_ctx : im_ctx_base {
-    unsigned int maxMemory;
-
-    unsigned int width;
-    unsigned int height;
-    unsigned int xoffset;
-    unsigned int yoffset;
-    bool strip;
-    bool trim;
-    bool autoOrient;
-    double trimFuzz;
-    std::string resizeStyle;
-    std::string gravity;
-    std::string format;
-    std::string filter;
-    std::string blur;
-    std::string background;
-    Magick::ColorspaceType colorspace;
-    unsigned int quality;
-    int rotate;
-    int density;
-    int flip;
-
-    convert_im_ctx() {}
-};
-// Extra context for composite
-struct composite_im_ctx : im_ctx_base {
-    char* compositeData;
-    size_t compositeLength;
-
-    std::string gravity;
-
-    composite_im_ctx() {}
-};
-
-
-inline Local<Value> WrapPointer(char *ptr, size_t length) {
+Local<Value> WrapPointer(char *ptr, size_t length) {
     Nan::EscapableHandleScope scope;
     return scope.Escape(Nan::CopyBuffer(ptr, length).ToLocalChecked());
 }
-inline Local<Value> WrapPointer(char *ptr) {
+Local<Value> WrapPointer(char *ptr) {
     return WrapPointer(ptr, 0);
 }
-
-
-#define RETURN_BLOB_OR_ERROR(req) \
-    do { \
-        im_ctx_base* _context = static_cast<im_ctx_base*>(req->data); \
-        if (!_context->error.empty()) { \
-            Nan::ThrowError(_context->error.c_str()); \
-        } else { \
-            const Local<Value> _retBuffer = WrapPointer((char *)_context->dstBlob.data(), _context->dstBlob.length()); \
-            info.GetReturnValue().Set(_retBuffer); \
-        } \
-        delete req; \
-    } while(0);
 
 
 bool ReadImageMagick(Magick::Image *image, Magick::Blob srcBlob, std::string srcFormat, im_ctx_base *context) {
--- src/imagemagick.h
+++ src/imagemagick.h
@@ -1,3 +1,6 @@
+#ifndef IMAGEMAGICK_H
+#define IMAGEMAGICK_H
+
 #include <Magick++.h>
 #include <node.h>
 #include <node_buffer.h>
@@ -6,3 +9,82 @@
 
 using namespace v8;
 using namespace node;
+
+// Base context for calls shared on sync and async code paths
+struct im_ctx_base {
+    Nan::Callback * callback;
+    std::string error;
+
+    char* srcData;
+    size_t length;
+    int debug;
+    int ignoreWarnings;
+    std::string srcFormat;
+
+    // generated blob by convert or composite
+    Magick::Blob dstBlob;
+
+    virtual ~im_ctx_base() {}
+};
+// Extra context for identify
+struct identify_im_ctx : im_ctx_base {
+    Magick::Image image;
+
+    identify_im_ctx() {}
+};
+// Extra context for convert
+struct convert_im_ctx : im_ctx_base {
+    unsigned int maxMemory;
+
+    unsigned int width;
+    unsigned int height;
+    unsigned int xoffset;
+    unsigned int yoffset;
+    bool strip;
+    bool trim;
+    bool autoOrient;
+    double trimFuzz;
+    std::string resizeStyle;
+    std::string gravity;
+    std::string format;
+    std::string filter;
+    std::string blur;
+    std::string background;
+    Magick::ColorspaceType colorspace;
+    unsigned int quality;
+    int rotate;
+    int density;
+    int flip;
+
+    convert_im_ctx() {}
+};
+// Extra context for composite
+struct composite_im_ctx : im_ctx_base {
+    char* compositeData;
+    size_t compositeLength;
+
+    std::string gravity;
+
+    composite_im_ctx() {}
+};
+
+Local<Value> WrapPointer(char *ptr, size_t length);
+Local<Value> WrapPointer(char *ptr);
+
+#define RETURN_BLOB_OR_ERROR(req) \
+    do { \
+        im_ctx_base* _context = static_cast<im_ctx_base*>(req->data); \
+        if (!_context->error.empty()) { \
+            Nan::ThrowError(_context->error.c_str()); \
+        } else { \
+            const Local<Value> _retBuffer = WrapPointer((char *)_context->dstBlob.data(), _context->dstBlob.length()); \
+            info.GetReturnValue().Set(_retBuffer); \
+        } \
+        delete req; \
+    } while(0);
+
+void AutoOrient(Magick::Image *image);
+
+bool ReadImageMagick(Magick::Image *image, Magick::Blob srcBlob, std::string srcFormat, im_ctx_base *context);
+
+#endif
